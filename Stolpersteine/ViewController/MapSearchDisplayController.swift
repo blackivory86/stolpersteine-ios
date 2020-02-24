7d50d61265e2aeed3ab1c8462d12a24666e63a07
Jan Rose <jan.rose@grandcentrix.net>
2020-02-24 20:59:01 +0100

    Fix crash on iOS 12

--- Stolpersteine/ViewController/MapSearchDisplayController.swift
+++ Stolpersteine/ViewController/MapSearchDisplayController.swift
@@ -24,6 +24,10 @@ class MapSearchDisplayController: UISearchController {
 //
 //        automaticallyShowsCancelButton = false
     }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
